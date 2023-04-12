9cc9e8da7491bd25960c15c71db35bae643b5fb8
BreadEnthusiast <dsw48698@student.dsw.edu.pl>
2023-04-12 23:55:54 +0200

    Cz. 1 trzeci myślnik

--- skrypt.sh
+++ skrypt.sh
@@ -6,3 +6,13 @@ if [ "$1" = "--date" ]; then
 else
 	echo "Użycie: $0 --date"
 fi
+
+if  [ "$1" = "--logs" ]; then
+	for i in {1..100}; do
+		filename="log$i.txt"
+		echo "$filename stworzony przez skrypt.sh dnia $today" >> $filename
+	done
+else
+	echo "Użycie: $0 --logs"
+fi
+
